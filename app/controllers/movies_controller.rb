6dd73cb9d218be17f7d658a1d9a5a5f3c6e9d630
William Hwang <willhwang90@gmail.com>
2022-07-20 11:07:24 -0400

    commit tree 2438b677ca236ba1011c27c724a4cf9d57c57833

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,13 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+  render json: Movie.create(movie_params), status: :created
+  end
+
+
+private
+  def movie_params
+      params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
 end
